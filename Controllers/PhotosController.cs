0e8ad09031428edc1a58144c95b0a5de033ef386
Lhar Gil <gil_lhar@live.com>
2021-01-09 14:24:34 +0800

    Add project files.

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    public class PhotoModel
+    {
+        public IFormFile Photo { get; set; }
+        public string Description { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IFileProvider _fileProvider;
+        private readonly ILogger<PhotosController> _logger;
+        private readonly string _targetFilePath;
+        private readonly long _fileSizeLimit;
+        private readonly string[] _permittedExtensions = { ".jpg" };
+
+        public PhotosController(IFileProvider fileProvider, ILogger<PhotosController> logger)
+        {
+            _fileProvider = fileProvider;
+            _fileSizeLimit = 2097152;
+
+            // To save physical files to a path provided by configuration:
+            _targetFilePath = Path.Combine((Environment.OSVersion.Platform == PlatformID.Unix ||
+                   Environment.OSVersion.Platform == PlatformID.MacOSX)
+                    ? Environment.GetEnvironmentVariable("HOME")
+                    : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%"), "omgimgflow_photos");
+            _logger = logger;
+        }
+        // GET: api/<PhotosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var files = _fileProvider.GetDirectoryContents("omgimgflow_photos");
+
+            return files.Select(f => WebUtility.HtmlDecode($"{f.Name}")).ToList();
+        }
+
+        // GET api/<PhotosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PhotosController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] PhotoModel photoModel)
+        {
+            var trustedFileNameForDisplay = "";
+            if (photoModel.Photo != null && photoModel.Photo.Length > 0)
+            {
+                var hasContentDispositionHeader =
+                    ContentDispositionHeaderValue.TryParse(
+                        photoModel.Photo.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                                contentDisposition.FileName);
+
+                    var trustedFileNameForFileStorage = contentDisposition.FileName; // Path.GetRandomFileName();
+
+                    var fileContent = await FileHelpers.ProcessFormFile<IFormFile>(photoModel.Photo, ModelState, _permittedExtensions, _fileSizeLimit);
+
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+
+                    using (var targetStream = System.IO.File.Create(
+                                Path.Combine(_targetFilePath, trustedFileNameForFileStorage.Replace("\"", ""))))
+                    {
+                        await targetStream.WriteAsync(fileContent);
+
+                        _logger.LogInformation(
+                            "Uploaded file '{TrustedFileNameForDisplay}' saved to " +
+                            "'{TargetFilePath}' as {TrustedFileNameForFileStorage}",
+                            trustedFileNameForDisplay, _targetFilePath,
+                            trustedFileNameForFileStorage);
+                    }
+                }                
+            }
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok();
+        }
+
+        // PUT api/<PhotosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PhotosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,121 @@
+using Imageflow.Server;
+using Imageflow.Server.HybridCache;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            HomeFolder = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                   Environment.OSVersion.Platform == PlatformID.MacOSX)
+                    ? Environment.GetEnvironmentVariable("HOME")
+                    : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+        }
+
+        public IConfiguration Configuration { get; }
+        public string HomeFolder { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin()
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                                  });
+            });
+
+            services.AddControllers();
+            // You can add a hybrid cache (in-memory persisted database for tracking filenames, but files used for bytes)
+            // But remember to call ImageflowMiddlewareOptions.SetAllowCaching(true) for it to take effect
+            // If you're deploying to azure, provide a disk cache folder *not* inside ContentRootPath
+            // to prevent the app from recycling whenever folders are created.
+            services.AddImageflowHybridCache(
+                new HybridCacheOptions(Path.Combine(HomeFolder, "omgimgflow_cache"))
+                {
+                    // How long after a file is created before it can be deleted
+                    MinAgeToDelete = TimeSpan.FromSeconds(10),
+                    // How much RAM to use for the write queue before switching to synchronous writes
+                    QueueSizeLimitInBytes = 100 * 1000 * 1000,
+                    // The maximum size of the cache (1GB)
+                    CacheSizeLimitInBytes = 1024 * 1024 * 1024,
+                });
+
+            var physicalProvider = new PhysicalFileProvider($"{HomeFolder}");
+
+            services.AddSingleton<IFileProvider>(physicalProvider);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else 
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseImageflow(new ImageflowMiddlewareOptions()
+                // Maps / to WebRootPath
+                .SetMapWebRoot(false)
+                //.SetMyOpenSourceProjectUrl("https://github.com/imazen/imageflow-dotnet-server")
+                // Maps /folder to WebRootPath/folder
+                .MapPath("/photos", Path.Combine(HomeFolder, "omgimgflow_photos"))
+                // Allow HybridCache or other registered IStreamCache to run
+                .SetAllowCaching(true)
+                // Cache publicly (including on shared proxies and CDNs) for 30 days
+                .SetDefaultCacheControlString("public, max-age=2592000")
+                .HandleExtensionlessRequestsUnder("/omgimgflow_photos/", StringComparison.OrdinalIgnoreCase)
+                .AddCommandDefault("down.filter", "mitchell")
+                .AddCommandDefault("f.sharpen", "15")
+                .AddCommandDefault("webp.quality", "80")
+                .AddCommandDefault("ignore_icc_errors", "true")
+                //When set to true, this only allows ?preset=value URLs, returning 403 if you try to use any other commands. 
+                .SetUsePresetsExclusively(false)
+                .AddPreset(new PresetOptions("large", PresetPriority.DefaultValues)
+                    .SetCommand("width", "1024")
+                    .SetCommand("height", "1024")
+                    .SetCommand("mode", "max")));
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("AllowAll");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
